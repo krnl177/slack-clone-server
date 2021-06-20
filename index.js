a4581346f409e65a7aedf14d18645e64c9020a29
krnl177 <learn.krunal@gmail.com>
2021-06-20 19:37:41 +0530

    [feature] basic crud operations development

--- index.js
+++ index.js
@@ -2,22 +2,37 @@ import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 // import typeDefs from './schema';
 import fs from 'fs';
-import resolvers from './resolvers';
-import { sequelize } from './models'
+import { sequelize, User, Team, Channel, Message } from './models'
+import path from 'path'
+import { loadFilesSync } from '@graphql-tools/load-files'
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge'
+import { print } from 'graphql'
 require("dotenv").config;
 
 const PORT = process.env.PORT || 3000;
-const path = '/graphql'
+const graphQLPath = '/graphql'
 const app = express();
 app.use(express.json())
 
-const typeDefs = gql(fs.readFileSync('./schema.graphql', {encoding: 'utf8'}))
+const typesArray = loadFilesSync(path.join(__dirname, './schemas'), { recursive: true })
+const typeDefs = mergeTypeDefs(typesArray)
+
+const resolversArray = loadFilesSync(path.join(__dirname, './resolvers'));
+const resolvers = mergeResolvers(resolversArray);
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: {
+        models : {
+            User, Team, Channel, Message
+        },
+        user: {
+            id: 1
+        }
+    },
 })
-server.applyMiddleware({ app, path })
+server.applyMiddleware({ app, path: graphQLPath })
 
 app.listen(PORT, async () => {
     console.log('server is running on', PORT);
